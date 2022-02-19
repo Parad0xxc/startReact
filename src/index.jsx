27bc649f4f8ac39369e3aa14d1208a4dc1d1eee7
Mayorov <mayorov.d@pos-credit.ru>
2022-02-19 23:48:47 +0300

    123

---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+  import * as React from 'react';
+  import * as ReactDOM from 'react-dom';
+
+  window.addEventListener('load', () => {
+      ReactDOM.render(<Header />, document.getElementById('react-root'))
+  })
