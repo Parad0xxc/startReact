3652787e7c9a3a34e275731d007ae5249a6015e9
Mayorov <mayorov.d@pos-credit.ru>
2022-02-19 23:22:15 +0300

    nah

--- src/index.jsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from './Header';
-import {Header} from "./Header";
-
-window.addEventListener('load', () => {
-    ReactDOM.render(Header, document.getElementById('react_root'))
-})
