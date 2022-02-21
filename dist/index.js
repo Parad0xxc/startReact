9b4b2d71a582737ff08ba8ca240228eff1830adc
Mayorov <mayorov.d@pos-credit.ru>
2022-02-21 20:00:50 +0300

    configged

--- dist/index.js
+++ dist/index.js
@@ -8,7 +8,26 @@
  */
 /******/ (() => { // webpackBootstrap
 /******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.jsx":
+/*!***********************!*\
+  !*** ./src/index.jsx ***!
+  \***********************/
+/***/ (() => {
+
+eval("\n\n\n//# sourceURL=webpack://startreact/./src/index.jsx?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
 /******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.jsx"]();
 /******/ 	
 /******/ })()
 ;
